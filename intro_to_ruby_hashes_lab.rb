3151c5a2b15ddd3a7b848164ee8a545144004849
Brandon Weaver <a7xncob@gmail.com>
2019-10-21 00:52:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,4 +33,5 @@ def monopoly_with_fourth_tier
   hash[:railroads][:names][:pennsylvania_railroad] = { "mortgage_value" => "$200" }
   hash[:railroads][:names][:b_and_o_railroad] = { "mortgage_value" => "$400" }
   hash[:railroads][:names][:shortline_railroad] = { "mortgage_value" => "$800" }
+  return hash
 end
