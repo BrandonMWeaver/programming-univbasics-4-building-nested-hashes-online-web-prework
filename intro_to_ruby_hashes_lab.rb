fb4f50d1d2250c62117875f3c6564ac6336052ed
Brandon Weaver <a7xncob@gmail.com>
2019-10-21 00:46:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,7 +18,9 @@ def monopoly_with_third_tier
     three_pieces_owned: 100,
     four_pieces_owned: 200
   }
-  hash[:railroads][:names] =
+  hash[:railroads][:names] = {
+    
+  }
   return hash
 end
 
